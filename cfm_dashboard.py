955da862d5cbd9ddf27fedc05b1460fc934d71af
Joonsoo (Kevin) Kim <37880418+snowjs0212@users.noreply.github.com>
2024-06-14 15:07:36 -0700

    Release changes and updates

--- cfm_dashboard.py
+++ cfm_dashboard.py
@@ -23,7 +23,7 @@ print(st.__version__)
 #fred_api_key_input = st.secrets["fred_api_key"]
 
 ### API key
-fred_api_key_input = "7a3a846e27994737590201d1ed80f0f1"
+fred_api_key_input = st.secrets["fred_api_key"]
 fred = Fred(api_key = fred_api_key_input)
 
 ### Global price of Coffee, Robustas
